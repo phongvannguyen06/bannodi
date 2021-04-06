ee015ddaefbb76d6b36207e0414dfac94bcd0f80
Phong Van Nguyen <phong_nguyen@hcl.com>
2021-04-06 10:43:46 -0400

    initial commit

---
+++ src/main/java/com/hcl/bannodi/BannodiApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.bannodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannodiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannodiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/bannodi/BannodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.bannodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BannodiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
