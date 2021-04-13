54d32f287932ad59dd497ba98f8d87f91fbc07e1
Phong Van Nguyen <phong_nguyen@hcl.com>
2021-04-12 21:51:28 -0400

    add product service starter files

---
+++ product-service/src/main/java/com/bannodi/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bannodi.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+}
