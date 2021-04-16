f16d2d2c60237c16f7def5898ef821902e94fa83
Phong Van Nguyen <phong_nguyen@hcl.com>
2021-04-16 15:37:25 -0400

    refactor data.sql file in User and Product services

--- product-service/src/main/resources/data.sql
+++ product-service/src/main/resources/data.sql
@@ -1,18 +1,10 @@
 INSERT INTO `bannodidb`.`product` (title, product_detail, price, quantity)
-VALUES ('Computer', 'computer product detail', 599.99, 3);
-INSERT INTO `bannodidb`.`product` (title, product_detail, price, quantity)
-VALUES ('Mask', 'mask product detail', 19.99, 100);
-INSERT INTO `bannodidb`.`product` (title, product_detail, price, quantity)
-VALUES ('Headphone', 'Headphone product detail', 299.99, 10);
-INSERT INTO `bannodidb`.`product` (title, product_detail, price, quantity)
-VALUES ('Phone', 'Phone product detail', 699.99, 10);
-INSERT INTO `bannodidb`.`product` (title, product_detail, price, quantity)
-VALUES ('Shirt', 'Shirt product detail', 10.00, 50);
-INSERT INTO `bannodidb`.`product` (title, product_detail, price, quantity)
-VALUES ('Frisbee', 'Frisbee product detail', 7.99, 50);
-INSERT INTO `bannodidb`.`product` (title, product_detail, price, quantity)
-VALUES ('Backpack', 'Backpack product detail', 99.99, 8);
-INSERT INTO `bannodidb`.`product` (title, product_detail, price, quantity)
-VALUES ('Stickers', 'Stickers product detail', 2.99, 100);
-INSERT INTO `bannodidb`.`product` (title, product_detail, price, quantity)
-VALUES ('Necklace', 'Necklace product detail', 399.99, 2);
+VALUES ('Computer', 'computer product detail', 599.99, 3),
+('Mask', 'mask product detail', 19.99, 100),
+('Headphone', 'Headphone product detail', 299.99, 10),
+('Phone', 'Phone product detail', 699.99, 10),
+('Shirt', 'Shirt product detail', 10.00, 50),
+('Frisbee', 'Frisbee product detail', 7.99, 50),
+('Backpack', 'Backpack product detail', 99.99, 8),
+('Stickers', 'Stickers product detail', 2.99, 100),
+('Necklace', 'Necklace product detail', 399.99, 2);
